287872286ce8b93e2abeee5db8a10051e75f32bf
odell <zhenhui.xu@duke.edu>
2020-11-09 00:38:20 -0500

    basic structure of the dashboard

---
+++ apps/home.py
@@ -0,0 +1,67 @@
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+import dash
+# needed only if running this as a single page app
+#external_stylesheets = [dbc.themes.LUX]
+
+#app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+# change to app.layout if running as single page app instead
+layout = html.Div([
+    dbc.Container([
+        dbc.Row([
+            dbc.Col(html.H1("Welcome to the MIMIC Predictive Modelling dashboard", className="text-center")
+                    , className="mb-5 mt-5")
+        ]),
+        dbc.Row([
+            dbc.Col(html.H5(children='This dashboard shows you the EDA and Modelling process of predicting dicharge types of patients '
+                                     )
+                    , className="mb-4")
+            ]),
+
+        dbc.Row([
+            dbc.Col(html.H5(children='The type of discharge location of patients is a important representation of healthcare utilizations. '
+                                     'Here, we use the demographics, vitals and laboratory data to predict four different types of discharge locations'
+                                     )
+                    , className="mb-5")
+        ]),
+
+        dbc.Row([
+            dbc.Col(dbc.Card(children=[html.H3(children='Get the original datasets used in this dashboard',
+                                               className="text-center"),
+                                       dbc.Button("MIMIC",
+                                                  href="https://mimic.physionet.org/",
+                                                  color="primary",className="mt-3"),
+                                       ],
+                             body=True, color="dark", outline=True)
+                    , width=4, className="mb-4"),
+
+            dbc.Col(dbc.Card(children=[html.H3(children='Access the code used to build this dashboard',
+                                               className="text-center"),
+                                       dbc.Button("GitHub",
+                                                  href="https://github.com/biostats823-final-project/MIMIC-Dashboard",
+                                                  color="primary",
+                                                  className="mt-3"),
+                                       ],
+                             body=True, color="dark", outline=True)
+                    , width=4, className="mb-4"),
+
+            dbc.Col(dbc.Card(children=[html.H3(children='Read the Github-Pages detailing the process',
+                                               className="text-center"),
+                                       dbc.Button("Github-Pages",
+                                                  #href="https://medium.com/@meredithwan",
+                                                  color="primary",
+                                                  className="mt-3"),
+
+                                       ],
+                             body=True, color="dark", outline=True)
+                    , width=4, className="mb-4")
+        ], className="mb-5")
+
+    ])
+
+])
+
+# needed only if running this as a single page app
+#if __name__ == '__main__':
+#    app.run_server(debug=True)
