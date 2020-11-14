ba422a14faa71a226ecfdb844ecc5b435792335d
ZhiQiu <qziuhi@126.com>
2020-11-14 07:27:16 -0500

    part of importance plot done

--- apps/Models.py
+++ apps/Models.py
@@ -11,6 +11,9 @@ from sklearn.metrics import confusion_matrix, f1_score, accuracy_score, precisio
 
 from app import app
 
+
+
+
 # function1
 def plot_confusion_matrix(cm, labels):
     '''
@@ -81,11 +84,71 @@ def plot_df(df_test, df_train):
     
     return fig
 
+# function 3
+def plot_imp_single(df, n):
+    '''
+    Function for plot feature importance for a single classifier.
+    
+    df: input feature importance dataframe (dataframe)
+    n : number of features to display (float)
+    '''
+    
+    df_new = df.sort_values(by = ['importance'], ascending=False)[:n]
+    df_new_ = df_new.sort_values(by = ['importance'])
+    
+    if n is None:
+        best = dict(height=2000, width=1000)
+    else:
+        s = n-1
+        if s in range(6):
+            best = dict(height=350, width=1000)
+        elif s in range(6, 11):
+            best = dict(height=500, width=1000)
+        elif s in range(11, 19):
+            best = dict(height=700, width=1000)
+        elif s in range(19, 36):
+            best = dict(height=1000, width=1000)
+        elif s in range(36, 44):
+            best = dict(height=1200, width=1000)
+        elif s in range(44, 51):
+            best = dict(height=1500, width=1000)
+        elif s in range(51, 61):
+            best = dict(height=1700, width=1000)
+        elif s in range(61, 71):
+            best = dict(height=1850, width=1000)
+        else:
+            best = dict(height=2000, width=1000)
+    
+    layout = dict(
+        title='Barplot of Feature Importance with top {} features'.format(n),
+        **best,
+        yaxis=dict(
+            showgrid=False,
+            showline=False,
+            showticklabels=True,
+            automargin=True
+        ))
+
+
+    fig = go.Figure(go.Bar(
+            x=[i for i in df_new_.importance],
+            y=[i for i in df_new_.features],
+            marker={
+            "color": [i for i in df_new_.importance], 
+            "colorscale": "Viridis", 
+            "reversescale": True
+          }, 
+            orientation='h'),
+                   layout=layout)
+
+    return fig
+
 
 
 # data
 df_train = pd.read_csv('./Data/train_results_decoded.csv', index_col=0)
 df_test = pd.read_csv('./Data/test_results_decoded.csv', index_col=0)
+
 
 layout = html.Div([
     dbc.Container([
@@ -95,8 +158,11 @@ layout = html.Div([
         dbc.Row([
             dbc.Col(html.H6(children='Visualization of model performance and feature importance.'), className="mb-4")
         ]),
-# choose between classifiers
-    html.Label(["Choose the model here: ", dcc.Dropdown(
+        
+     # choose between classifiers
+     html.Label(["Select a model: "]), 
+                
+     dcc.Dropdown(
         id='classifier',
         options=[
             {'label': 'Dummy Classifier', 'value': 'dummy'}, 
@@ -109,8 +175,9 @@ layout = html.Div([
         value='xgb',
         #multi=True,
         #style={'width': '50%'}
-        ) ]),
-# for some reason, font colour remains black if using the color option
+        ), 
+        
+    # for some reason, font colour remains black if using the color option
     dbc.Row([
         dbc.Col(dbc.Card(html.H3(children='Target Variable: Discharge Locations',
                                  className="text-center text-light bg-dark"), body=True, color="dark")
@@ -126,6 +193,26 @@ layout = html.Div([
         dbc.Col(dcc.Graph(id='conf1'), width=7), 
         dbc.Col(dcc.Graph(id='table1'), width=5) 
         ]),
+    
+    # feature importance part
+    dbc.Row([
+        dbc.Col(dbc.Card(html.H3(children='Feature Importance Exploration',
+                                 className="text-center text-light bg-dark"), body=True, color="dark")
+        , className="mt-4 mb-4")
+    ]),    
+    
+    html.Label(["Number of Top features: (minimum 1, maximum 84)"]),
+    html.Br(),
+        
+    dcc.Input(
+            id="num_of_feature", type="number", placeholder='Select or enter...',
+            min=1, max=84,
+        ),
+    
+    dbc.Row([
+        dbc.Col(dcc.Graph(id='imp1'), width=12)
+        ]),
+    
 
 ])
 
@@ -133,10 +220,11 @@ layout = html.Div([
 ])
 
 @app.callback([Output('conf1', 'figure'),
-               Output('table1', 'figure')],
-              [Input('classifier', 'value')])
+               Output('table1', 'figure'),
+               Output('imp1', 'figure')],
+              [Input('classifier', 'value'), Input('num_of_feature', 'value')])
 
-def update_graph(classifier):
+def update_graph(classifier, num_of_feature):
     
     target_label = ['DEAD/EXPIRED', 'HOME', 'OTHERS', 'SNF']
     
@@ -170,21 +258,31 @@ def update_graph(classifier):
         df_fig2 = df_train.copy().iloc[:, [0,5]]
         cm = confusion_matrix(df_fig.iloc[:,0], df_fig.iloc[:,1])
         cm_percent = (cm / cm.sum(axis=1, keepdims=True)).round(2)
+        df_imp = pd.read_csv('./Data/xgb_importance.csv', index_col=0)
         
     else:
         df_fig = df_test.copy().iloc[:, [0,6]]
         df_fig2 = df_train.copy().iloc[:, [0,6]]
         cm = confusion_matrix(df_fig.iloc[:,0], df_fig.iloc[:,1])
         cm_percent = (cm / cm.sum(axis=1, keepdims=True)).round(2)
+        df_imp = pd.read_csv('./Data/rf_importance.csv', index_col=0)
         
-
+      
     # figure 1, confusion matrix
     fig = plot_confusion_matrix(cm_percent, target_label)
     
     # figure 2, table
     fig2 = plot_df(df_fig, df_fig2)
+    
+    # figure 3, importance
+    fig3 = plot_imp_single(df_imp, num_of_feature)
 
-    return fig, fig2
+    return fig, fig2, fig3
+
+
+
+
+
 
 # needed only if running this as a single page app
 #if __name__ == '__main__':
